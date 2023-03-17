405125b4549af6d477cf978c3540ac33c42f386b
DubemObi <dubemobiwon@gmail.com>
2023-03-17 05:51:52 -0700

    Commit

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ dotenv.config({ path: "./config.env" });
 const app = require("./index");
 const { PORT, DB } = process.env;
 
-mongoose.connect(process.env.DB).then(() => {
-  console.log("Connected to Database");
-});
+mongoose
+  .connect(process.env.DB)
+  .then(() => {
+    console.log("Connected to Database");
+  })
+  .catch((err) => console.log(err));
 app.listen(PORT, () => {
   console.log("App is running on port " + PORT);
 });
